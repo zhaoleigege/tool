a0a76f865c976c723b0d5a9f9711ecb277041eea
buse <buse.zhao@klook.com>
2020-11-10 18:08:26 +0800

    添加错误产生的cause

--- errors/errors.go
+++ errors/errors.go
@@ -9,20 +9,27 @@ import (
 
 type bErr struct {
 	message string    // 这一次错误的信息描述
-	preErr  *bErr     // 指向上一个错误
+	preErr  error     // 指向上一个错误
 	stack   []uintptr // 调用栈
 }
 
 // Error 实现了error接口
 func (e *bErr) Error() string {
-
 	buf := bytes.Buffer{}
 
 	pcs := make([]uintptr, 0)
 	msgs := make([]string, 0)
-	for innerErr := e; innerErr != nil; innerErr = innerErr.preErr {
+	for innerErr := e; innerErr != nil; {
 		pcs = innerErr.stack
 		msgs = append(msgs, innerErr.message)
+		preErr := innerErr.preErr
+
+		cause, ok := preErr.(*bErr)
+		if !ok {
+			break
+		}
+
+		innerErr = cause
 	}
 
 	for i, pc := range pcs {
@@ -40,13 +47,19 @@ func (e *bErr) Error() string {
 			msg = msgs[index]
 		}
 
-		buf.WriteString(fmt.Sprintf("\t %s:%d %s %s\n", file, line, f.Name(), msg))
+		buf.WriteString(fmt.Sprintf("\n\t %s:%d %s %s", file, line, f.Name(), msg))
 	}
 
 	return buf.String()
 }
 
+// Format 实现了Formatter接口，可以自定义格式化输入输出的内容
+func (e *bErr) Format(s fmt.State, c rune) {
+	_, _ = s.Write([]byte(e.Error()))
+}
+
 // New 返回一个包含了原始error的新error对象
+// 让一开始error记录调用的堆栈信息
 func New(err error, msg string) error {
 	if err, ok := err.(*bErr); ok {
 		return &bErr{
@@ -57,7 +70,7 @@ func New(err error, msg string) error {
 
 	return &bErr{
 		message: msg,
-		preErr:  nil,
+		preErr:  err,
 		stack:   errCallers(),
 	}
 }
@@ -75,9 +88,23 @@ func NewF(err error, format string, a ...interface{}) error {
 
 	return &bErr{
 		message: msg,
-		preErr:  nil,
+		preErr:  err,
 		stack:   errCallers(),
 	}
+}
+
+// Cause 返回原始的错误类型
+func Cause(err error) error {
+	for err != nil {
+		cause, ok := err.(*bErr)
+		if !ok {
+			break
+		}
+
+		err = cause.preErr
+	}
+
+	return err
 }
 
 // errCallers 返回bErr生成的函数调用栈
--- errors/example/err1.go
+++ errors/example/err1.go
@@ -6,6 +6,15 @@ import (
 	"sync"
 )
 
+type NameErr struct {
+	Name string
+	Age  int64
+}
+
+func (e *NameErr) Error() string {
+	return fmt.Sprintf("%s:%d", e.Name, e.Age)
+}
+
 func main() {
 	Err2()
 }
@@ -23,7 +32,19 @@ func Err2() {
 
 func Err1() {
 	err := foo()
-	fmt.Printf("出现错误: %s", err)
+	fmt.Printf("出现错误: %s\n", err)
+	fmt.Printf("出现错误: %v\n", err)
+	switch e := errors.Cause(err).(type) {
+	case *NameErr:
+		fmt.Printf("NameErr: %s: %d\n", e.Name, e.Age)
+	default:
+		switch e {
+		case errors.NotExist:
+			fmt.Printf("原始错误是NotExist")
+		}
+	}
+
+	fmt.Printf("最里面的错误: %v\n", errors.Cause(err))
 }
 
 func foo() error {
@@ -38,7 +59,11 @@ func foo() error {
 func bar() error {
 	fmt.Println("bar")
 	return func() error {
-		err := errors.New(nil, "bar发生了错误")
+		//err := errors.New(errors.NotExist, "bar发生了错误")
+		err := errors.New(&NameErr{
+			Name: "test",
+			Age:  20,
+		}, "bar发生了错误")
 		return errors.New(err, "闭包函数错误")
 	}()
 }
